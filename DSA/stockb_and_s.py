42f34554df748d24b4eb7227693647b40cbe7806
Surbhi Sharma <surbhisharma1581@gmail.com>
2025-06-14 21:09:55 +0530

    Add files via upload

---
+++ DSA/stockb_and_s.py
@@ -0,0 +1,10 @@
+def maxProfit(prices):
+    profit = 0
+    for i in range(1, len(prices)):
+        if prices[i] > prices[i - 1]:
+            profit += prices[i] - prices[i - 1]
+    return profit
+
+# Example input
+prices = [100, 180, 260, 310, 40, 535, 695]
+print("Maximum Profit:", maxProfit(prices))
