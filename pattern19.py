85942b1451237f57d391862d8079cc37933e307b
surbhisharma2762 <surbhisharma1581@gmail.com>
2025-08-25 20:13:58 +0530

    Adding pattern19

---
+++ pattern19.py
@@ -0,0 +1,42 @@
+# **********
+# ****  ****
+# ***    ***
+# **      **
+# *        *
+# *        *
+# **      **
+# ***    ***
+# ****  ****
+# **********
+def pattern19(n):
+    for i in range(n):
+        # stars
+        for j in range(n-i):
+            print('*',end="")
+        # spaces
+        for j in range(2*i):
+            print(" ",end="")
+
+        # stars
+        for j in range(n-i):
+            print('*',end="")
+        print()
+
+    for i in range(n):
+        # stars (left)
+        for j in range(i + 1):
+            print('*', end="")
+
+        # spaces (middle)
+        for j in range(2 * (n - i - 1)):
+            print(" ", end="")
+
+        # stars (right)
+        for j in range(i + 1):
+            print('*', end="")
+        print()
+
+t = int(input())
+for i in range(t):
+    n = int(input())
+    pattern19(n)
