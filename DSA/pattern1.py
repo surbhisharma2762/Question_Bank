249cf89cbc0354451b2d49d8147a85d6cea9a1f3
Surbhi Sharma <surbhisharma1581@gmail.com>
2025-08-29 07:22:15 +0530

    Add files via upload
    Pattern1 N-Forest

---
+++ DSA/pattern1.py
@@ -0,0 +1,9 @@
+def print1(n):
+    for i in range(n):
+        for j in range(n):
+            print('*',end="")
+        print()
+t=int(input())
+for t in range(t):
+    n=int(input())
+    print1(n)
